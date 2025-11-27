4333b85ab74bdaeccf8203ccd96d4a79b649fed0
b.sajai <b.sajai@perenitysoftware.com>
2025-11-27 18:03:32 +0000

    master commit

---
+++ env-mgmt-frontend/.angular/cache/21.0.1/env-mgmt-frontend/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,52 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-G2MZHMLF.js";
+import "./chunk-3UDKHAWZ.js";
+import "./chunk-GUGIMSVJ.js";
+import "./chunk-4QLU5MFL.js";
+import "./chunk-HGIXQTMX.js";
+import {
+  Dir
+} from "./chunk-LGOJHUOV.js";
+import "./chunk-JDI3SQXM.js";
+import "./chunk-UC72YTJX.js";
+import "./chunk-4ZVEXLGF.js";
+import "./chunk-4YCCEXQQ.js";
+import "./chunk-J46EEYGT.js";
+import "./chunk-U7EDC2PH.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
